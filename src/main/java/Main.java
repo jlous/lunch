4ad599ee78e00220daf336183c9ab8b942619151
Lous, Joachim <Joachim.Lous@skatteetaten.no>
2018-02-16 12:29:34 +0100

    hei

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,8 @@
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.apache.pdfbox.cos.COSDocument;
 import org.apache.pdfbox.io.RandomAccessBufferedFileInputStream;
@@ -7,8 +10,10 @@ import org.apache.pdfbox.pdfparser.PDFParser;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
 
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+
 import io.undertow.Undertow;
-import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
 import io.undertow.util.Headers;
 
@@ -17,26 +22,58 @@ public class Main {
     public static void main(final String[] args) {
         Undertow server = Undertow.builder()
             .addHttpListener(8080, "localhost")
-            .setHandler(new HttpHandler() {
-                public void handleRequest(final HttpServerExchange exchange) {
-                    exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
-                    exchange.getResponseSender().send(parsePdf(inputStream("Ukesmeny uke 7 2018 Bonusen 1 etg.pdf")));
-                }
-            }).build();
+            .setHandler(Main::handleRequest).build();
         server.start();
     }
 
-    public static String parsePdf(InputStream input) {
+    private static void handleRequest(HttpServerExchange exchange) {
+        exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain; charset=UTF-8");
+        exchange.getResponseSender().send(dagensMenyer());
+    }
+
+    private static String dagensMenyer() {
+        return menyLenker(getAsString("http://www.google.com/"))
+            .map(Main::getAsInputStream)
+            .map(Main::toText)
+            .map(Main::bareDagens)
+            .collect(Collectors.joining());
+    }
+
+    private static String bareDagens(String ukemeny) {
+        return null;
+    }
+
+    private static InputStream getAsInputStream(URL url) {
+        return null;
+    }
+
+    private static Stream<URL> menyLenker(String html) {
+        return null;
+    }
+
+    private static String getAsString(String url) {
         try {
-            PDFParser parser = new PDFParser(new RandomAccessBufferedFileInputStream(input));
+            return Unirest.get(url).asString().getBody();
+        } catch (UnirestException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static String toText(InputStream pdf) {
+        try {
+            PDFParser parser = new PDFParser(new RandomAccessBufferedFileInputStream(pdf));
             parser.parse();
             try (COSDocument cosDoc = parser.getDocument(); PDDocument pdDoc = new PDDocument(cosDoc)) {
                 return new PDFTextStripper().getText(pdDoc);
             }
         } catch (IOException e) {
-            e.printStackTrace();
-            return e.getMessage();
+            throw new RuntimeException(e);
         }
+    }
+
+    public static String fetch() throws UnirestException {
+        Unirest.get("http://www.google.com/").asBinary();
+        return "";
     }
 
     private static InputStream inputStream(String filename) {
